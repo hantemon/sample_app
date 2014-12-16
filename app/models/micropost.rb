01772ef81534ddca5bf7a98b05f1f0e70f6d3704
hantemon <mikle.elbravo@gmail.com>
2014-12-16 10:26:00 +0400

    Add user following works

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,7 +5,6 @@ class Micropost < ActiveRecord::Base
 	validates :user_id, presence: true
 
 	# Returns microposts from the users being followed by the given user.
-	# Для бОльших сайтов, вам, вероятно, потребуется генерировать поток асинхронно с помощью фонового процесса.
 	def self.from_users_followed_by(user)
 		# [1, 2, 3, 4].map { |i| i.to_s }
 		# [1, 2, 3, 4].map(&:to_s)
@@ -17,11 +16,12 @@ class Micropost < ActiveRecord::Base
 		# ассоциации has_many :followed_users (Листинг 11.10); в результате, для получения 
 		# id соответствующих коллекции user.followed_users, нам достаточно добавить _ids 
 		# к названию ассоциации.
-    	followed_user_ids = "SELECT followed_id FROM relationships
-                     		 WHERE follower_id = :user_id"
+    	#followed_user_ids = user.followed_user_ids
     	# where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
     	# рефакторинг для большого числа сообщений
-    	where("user_id IN (:followed_user_ids) OR user_id = :user_id",
-          	   followed_user_ids: followed_user_ids, user_id: user)
+    	followed_user_ids = "SELECT followed_id FROM relationships
+                         	 WHERE follower_id = :user_id"
+    	where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
+          	   user_id: user.id)
   	end
 end
